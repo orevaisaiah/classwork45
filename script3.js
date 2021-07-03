2c952edfa29e4ce8d4c8de3cd52c5107a5686a6e
inabororevaoghene@gmail.com <inabororevaoghene@gmail.com>
2021-07-03 11:55:32 +0100

    added new files

--- script3.js
+++ script3.js
@@ -1,8 +1,33 @@
-const naira = document.getElementById('naira');
-naira.addEventListener('input', nairaConverter);
-function nairaConverter(e){
-    let converterNaira = e.target.value;
-    document.getElementById('dollars').value = converterNaira / 493;
-    document.getElementById('pounds').value = converterNaira / 710;
-    document.getElementById('euros').value = converterNaira / 595;
+// const naira = document.getElementById('naira');
+// naira.addEventListener('input', nairaConverter);
+// function nairaConverter(e){
+//     let converterNaira = e.target.value;
+//     document.getElementById('dollars').value = converterNaira / 493;
+//     document.getElementById('pounds').value = converterNaira / 710;
+//     document.getElementById('euros').value = converterNaira / 595;
+// }
+
+// const password1 = document.form.p1;
+// const password2 = document.form.p2;
+
+// const submitBtn = document.querySelector('#submit');
+// submitBtn.addEventListener('click', function(e){
+//     e.preventDefault()
+//     if (password1.value !== password2.value){
+//         alert('Password does not match');
+//         return false;
+//     }
+//     else{
+//         alert('form submitted');
+//     }
+// });
+function checker(){
+    const numb = document.querySelector('#number').value;
+    if (isNaN(numb)){
+        alert('Please Enter a number');
+    }
+    else{
+        alert('form submitted');
+        console.log('form submitted');
+    }
 }
